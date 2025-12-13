27034c9547886ded93578c0506ae3b68a0201e1b
Marcos Barrera <marcosbarrera445@gmail.com>
2025-12-13 17:21:40 -0600

    1.11 unicafe step 6 complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -30,7 +30,8 @@ const App = () => {
       <Button onClick = {() => setNeutralTo(neutral+1)} text='Neutral'></Button>
       <Button onClick = {() => setBadTo(bad+1)} text='Bad'></Button>
       <h1>Statistics </h1>
-      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+      {/*<Statistics good={good} neutral={neutral} bad={bad}></Statistics>*/}
+      <Table good={good} neutral={neutral} bad={bad}></Table>
     </div>
   )
 }
@@ -47,12 +48,12 @@ const Statistics = ({good, neutral, bad}) => {
   if (good > 0 || neutral > 0 || bad > 0){ 
     return  (
       <div>
-        <StatisticLine text="Good " value={good}></StatisticLine>
-        <StatisticLine text="Neutral " value={neutral}></StatisticLine>
-        <StatisticLine text="Bad " value={bad}></StatisticLine>
-        <StatisticLine text="All  " value={good + bad + neutral}></StatisticLine>
-        <StatisticLine text="Average " value={(((good*1)+(bad*-1))/(good+neutral+bad))}></StatisticLine>
-        <StatisticLine text="Percentage " value={((good)/(good+neutral+bad))*100} unit="%"></StatisticLine>
+          <StatisticLine text="Good " value={good}></StatisticLine>
+          <StatisticLine text="Neutral " value={neutral}></StatisticLine>
+          <StatisticLine text="Bad " value={bad}></StatisticLine>
+          <StatisticLine text="All  " value={good + bad + neutral}></StatisticLine>
+          <StatisticLine text="Average " value={(((good*1)+(bad*-1))/(good+neutral+bad))}></StatisticLine>
+          <StatisticLine text="Percentage " value={((good)/(good+neutral+bad))*100} unit="%"></StatisticLine>
       </div>
     )
   }
@@ -64,5 +65,25 @@ const Statistics = ({good, neutral, bad}) => {
 const StatisticLine = ({text, value, unit}) => (
   <Display text={text} value={value} unit={unit}></Display>
 )
+
+const Table = ({good, neutral, bad}) => (
+   <table>
+        <tbody>
+          <TableRow text="Good" value={good}></TableRow>
+          <TableRow text="Neutral" value={neutral}></TableRow>
+          <TableRow text="Bad" value={bad}></TableRow>
+          <TableRow text="All" value={good + bad + neutral}></TableRow>
+          <TableRow text="Average " value={(((good*1)+(bad*-1))/(good+neutral+bad))}></TableRow>
+          <TableRow text="Percentage " value={((good)/(good+neutral+bad))*100} unit="%"></TableRow>
+        </tbody>
+      </table>
+)
+
+const TableRow = ({text, value, unit}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}{unit}</td>
+  </tr>
+) 
 
 export default App
