fec4a1ea818546a7cbe8b71ac2ed3d98302250dd
Marcos Barrera <marcosbarrera445@gmail.com>
2026-02-07 17:47:53 -0600

    completed 3.3

--- part3/exercises3.1-3.6/index.js
+++ part3/exercises3.1-3.6/index.js
@@ -35,7 +35,12 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(p => p.id === id)
-    response.json(person)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
@@ -47,6 +52,7 @@ app.get('/info', (request, response) => {
         </div>`
     )
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
