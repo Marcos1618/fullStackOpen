ded45f5f755430098d6751aaf5b94c1aa472a683
Marcos Barrera <marcosbarrera445@gmail.com>
2026-02-07 18:10:36 -0600

    complete 3.4

--- part3/exercises3.1-3.6/index.js
+++ part3/exercises3.1-3.6/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -53,6 +53,12 @@ app.get('/info', (request, response) => {
     )
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
