6ccd0f2457161491da45c51b807d744ac766140f
Marcos Barrera <marcosbarrera445@gmail.com>
2026-04-09 17:09:59 -0500

    added notes validation in schema and error handler

--- part3/notesP3/backend/index.js
+++ part3/notesP3/backend/index.js
@@ -11,8 +11,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  } else if (error.name === "ValidationError") {
+      return response.status(400).json({error: error.message})
+  }
   next(error)
 }
 
@@ -53,24 +54,19 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
   })
-
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
